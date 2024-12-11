f5b1602e7eb39851b7288c60a560e4cfc7941fe7
Ganesh Patil <ganeshpatil67284@gmail.com>
2024-12-11 15:02:42 +0530

    signature pad add

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  signaturePad:any;
+
+  constructor() { }
+  ngOnInit(): void {
+    let canvas: any = document.querySelector("canvas");
+    this.signaturePad = new SignaturePad(canvas, {
+      minWidth: 1,
+      maxWidth: 2,
+      penColor: "rgb(66, 133, 244)"
+    });
+   
+  }
+
+  saveSignature(){
+    this.signaturePad.toDataURL(); // save image as PNG
+    console.log(this.signaturePad.toDataURL()); 
+  }
+
+  Clear(){
+    this.signaturePad.clear();
+    this.signaturePad.isEmpty();
+    this.signaturePad.off();
+    this.signaturePad.on();
+  }
 }
